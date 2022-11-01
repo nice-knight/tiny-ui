d0a77ea3cfc61a665c2385993e86ecc7295d49c2
knight.gao <knightgao2@gmail.com>
2022-11-01 20:29:37 +0800

    chore: 修改目录结构，修复tscofig的配置

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+// 引入Unocss
+import Unocss from 'unocss/vite';
+import { presetUno, presetAttributify, presetIcons } from 'unocss';
+
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+export default defineConfig({
+    plugins: [
+        vueJsx(),
+        // 使用Unocss
+        Unocss({
+            presets: [
+                presetUno(),
+                presetAttributify(),
+                presetIcons(),
+            ],
+            // rules可以新增规则, 先放置为以后需要准备
+            rules: []
+        })
+    ],
+    server: {
+        fs: {
+            // Allow serving files from one level up to the project root
+            allow: ['../../']
+        }
+    }
+});
+
