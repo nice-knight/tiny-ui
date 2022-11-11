e7d051ee7af2649d2bb69de71bf035a2c74768a0
kity.wang <kity.wang@tineco.com>
2022-11-11 14:37:34 +0800

    fea:配置文件抽取

---
+++ packages/ui/docs/.vitepress/configs/head.ts
@@ -0,0 +1,48 @@
+//import type { HeadConfig } from '@vuepress/core';
+
+export const  head  = [
+  //顶部logo  TODO
+  [
+    'link', {rel: 'icon', href:'/images/logo.png'}
+  ],
+  // 覆盖全局vitepress样式
+  [
+    'link', {rel: 'stylesheet', href:'/css/index.css'}
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: `/images/icons/favicon-16x16.png`,
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: `/images/icons/favicon-32x32.png`,
+    },
+  ],
+  ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+  ['meta', { name: 'application-name', content: 'VuePress' }],
+  ['meta', { name: 'apple-mobile-web-app-title', content: 'VuePress' }],
+  ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+  [
+    'link',
+    { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+  ],
+  [
+    'link',
+    {
+      rel: 'mask-icon',
+      href: '/images/icons/safari-pinned-tab.svg',
+      color: '#3eaf7c',
+    },
+  ],
+  ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+  ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+]
---
+++ packages/ui/docs/.vitepress/configs/index.ts
@@ -0,0 +1,3 @@
+//export * from './head.ts'
+export * from './navbar/index.js'
+export * from './sidebar/index.js'
