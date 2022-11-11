e49fa9bead34493eb40e3bf9181b178aa1607f09
kity.wang <kity.wang@tineco.com>
2022-11-11 16:11:04 +0800

    fix: 引入错误

--- packages/ui/src/Button/index.ts
+++ packages/ui/src/Button/index.ts
@@ -3,7 +3,7 @@ import Button from "./src/Button.vue";
 
 export const ButtonPlugin: Plugin = {
     install(app: App) {
-        app.component("TinyButton", Button);
+        app.component("TinyButton", Button as any);
     },
 };
 
