75e94bd99e528345afa345a120c819409f3727d5
knight.gao <knightgao2@gmail.com>
2022-11-21 21:54:21 +0800

    feat: 完成初始化的打包配置

---
+++ packages/ui/scripts/build.mjs
@@ -0,0 +1,76 @@
+import { defineConfig, build } from 'vite';
+import {resolve,dirname} from "path";
+// 引入Unocss
+import Unocss from 'unocss/vite';
+import { presetUno, presetAttributify, presetIcons } from 'unocss';
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vue  from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+
+// 入口文件
+const entryFile = resolve(__dirname, '../src/index.ts');
+
+// 输出目录
+const outputDir = resolve(__dirname, '../dist');
+
+// 基础配置
+const baseConfig = defineConfig({
+    configFile: false,
+    publicDir: false,
+    plugins: [
+        vue(),
+        vueJsx(),
+        // 使用Unocss
+        Unocss({
+            presets: [
+                presetUno(),
+                presetAttributify(),
+                presetIcons(),
+            ],
+            // rules可以新增规则, 先放置为以后需要准备
+            rules: []
+        })
+    ],
+    build: {
+        sourcemap: true
+    },
+});
+
+// rollup配置
+const rollupOptions = {
+    // 外置
+    external: ['vue', 'vue-router'],
+    output: {
+        globals: {
+            vue: 'Vue'
+        }
+    }
+};
+// 执行创建
+// 全量构建
+const buildAll = async () => {
+    await build(
+        defineConfig({
+            ...baseConfig,
+            build: {
+                rollupOptions,
+                lib: {
+                    entry: entryFile,
+                    name: 'tiny-ui',
+                    fileName: 'tiny-ui',
+                    formats: ['es']
+                },
+                outDir: outputDir
+            }
+        })
+    );
+};
+
+
+buildAll();
--- packages/ui/src/Button/src/Button.vue
+++ packages/ui/src/Button/src/Button.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-    
+
 </script>
 <template>
   <div class="Button">
-    <slot name="default"></slot>
+    <slot name="default" />
   </div>
 </template>
 <style lang="scss">
